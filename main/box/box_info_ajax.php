8d0eda550e55e34bf46dc9b79275bcfde8f2375d
ipAli <98942611+ipAli@users.noreply.github.com>
2022-02-03 13:03:43 +0300

    Create box_info_ajax.php

---
+++ main/box/box_info_ajax.php
@@ -0,0 +1,15 @@
+<?php
+include_once '../../connect.php';
+
+$id = $_POST['id'];
+$query = "SELECT * FROM `boxs` WHERE `id` = '$id'";
+$result = $mysqli->query($query);
+while($base = $result->fetch_assoc())
+{
+    $data = array(
+    	'id'=>$base['id'],
+    	'box_number'=>$base['box_number']
+    );
+}
+echo json_encode($data);
+
